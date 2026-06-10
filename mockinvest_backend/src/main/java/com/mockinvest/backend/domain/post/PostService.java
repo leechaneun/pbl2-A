4f9ba577761b91442974994d0a1021dcd92cb2d2
이찬은 <leechaneun@ichan-eun-ui-MacBookAir.local>
2026-06-10 10:10:54 +0900

    게시판 수정

--- mockinvest_backend/src/main/java/com/mockinvest/backend/domain/post/PostService.java
+++ mockinvest_backend/src/main/java/com/mockinvest/backend/domain/post/PostService.java
@@ -3,6 +3,8 @@ package com.mockinvest.backend.domain.post;
 
 import com.mockinvest.backend.domain.mission.MissionService;
 import com.mockinvest.backend.domain.mission.MissionType;
+import com.mockinvest.backend.domain.trade.Trade;
+import com.mockinvest.backend.domain.trade.TradeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,8 +17,16 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final MissionService missionService;//미션 추가
+    private final TradeRepository tradeRepository;
     //게시글 작성
     public String createPost(Post post) {
+        // 1. 유저의 loginId로 주식/거래 내역을 DB에서 직접 조회합니다.
+        List<Trade> myTrades = tradeRepository.findByLoginId(post.getAuthor());
+
+        // 수정: 하나도 보유한 주식이 없으면 게시글 작성 안됨
+        if (myTrades == null || myTrades.isEmpty()) {
+            throw new IllegalArgumentException("보유한 주식에 대해서만 글을 작성할 수 있습니다.");
+        }
         String postId = postRepository.save(post).getPostId();
         missionService.completeMission(post.getAuthor(), MissionType.POST); //미션추가(게시글 작성)
         return postId;
